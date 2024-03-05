474c07d2417acbe6ee2686a4aba2db9d48453781
Siddhant <siddhantsocool10@gmail.com>
2024-03-05 12:08:54 +0530

    Edited Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,26 @@
 const Card = () => {
 	return (
 		<>
-			<div className="card w-96 bg-base-100 shadow-xl">
-				<figure>
-					<img
-						src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-						alt="Shoes"
-					/>
-				</figure>
-				<div className="card-body">
-					<h2 className="card-title">Shoes!</h2>
-					<p>If a dog chews shoes whose shoes does he choose?</p>
-					<div className="card-actions justify-end">
-						<button className="btn btn-primary">Buy Now</button>
+			
+				<div className="card w-96 bg-base-100 shadow-xl ml-[350px] mt-20">
+					<figure>
+						<img src="src/assets/games/hellblade.jpg" alt="Shoes" />
+					</figure>
+					<div className="card-body">
+						<h2 className="card-title">
+							Hellblade: Senua's Sacrifice{" "}
+						</h2>
+						<p>
+							Set in the Viking age, a broken Celtic warrior
+							embarks on a haunting vision quest into Viking Hell
+							to fight for the soul of her dead lover.{" "}
+						</p>
+						<div className="card-actions justify-end">
+							<button className="btn btn-primary">Buy Now</button>
+						</div>
 					</div>
 				</div>
-			</div>
+				
 		</>
 	);
 };
